859d1521334277b1928efa1aca2b0de8e7e587af
triveni <IT@IT-15>
2022-01-19 17:28:44 +0530

    Add project files.

---
+++ fractional/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace fractional
+{
+    class program
+    {
+        public void fractionals()
+        {
+            double value = 7.5;
+            int whole = 9;
+            double fractional = 8.0;
+            Console.WriteLine(value);
+
+            fractional = value - whole;
+            Console.WriteLine(whole);
+            Console.WriteLine(fractional);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program obj = new Program();
+            obj.fractional();
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ fractional/fractional.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace fractional
+{
+    class Program
+    {
+        public void fractional()
+        {
+            double value = 7.5;
+            int whole = 9;
+            double fractional = 8.0;
+            Console.WriteLine(value);
+           
+            fractional = value - whole;
+            Console.WriteLine(whole);
+            Console.WriteLine(fractional);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program obj = new Program();
+            obj.fractional();
+            Console.ReadLine();
+        }
+    }
+}
